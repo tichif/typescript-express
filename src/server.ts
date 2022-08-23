53049a5e84c10459023dc4ea708fd6a05ab1ba1e
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-08-23 14:56:08 -0400

    Setup the server

--- src/server.ts
+++ src/server.ts
@@ -13,8 +13,72 @@ mongoose
     retryWrites: true,
     w: 'majority',
   })
-  .then(() => Logging.info('Database connected'))
+  .then(() => {
+    Logging.info('Database connected');
+    startServer();
+  })
   .catch((err) => {
     Logging.error('Unable to connect:');
     Logging.error(err);
   });
+
+// Only start the server when database is connected
+const startServer = () => {
+  server.use((req, res, next) => {
+    // Log the request
+    Logging.info(
+      `INCOMING -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+    );
+
+    res.on('finish', () => {
+      // Log the response
+      Logging.info(
+        `OUTCOMING -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`
+      );
+    });
+    next();
+  });
+
+  server.use(express.urlencoded({ extended: true }));
+  server.use(express.json());
+
+  // CORS
+  server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, PATCH, DELETE, GET'
+      );
+      return res.status(200).json({});
+    }
+
+    next();
+  });
+
+  // Routes
+
+  // Healthcheck
+  server.get('/ping', (req, res, next) => {
+    return res.status(200).json({ message: 'pong' });
+  });
+
+  // Error Handling
+  server.use((req, res, next) => {
+    const error = new Error('Not Found');
+    Logging.error(error);
+
+    return res.status(404).json({ error: error.message });
+  });
+
+  http
+    .createServer(server)
+    .listen(config.server.port, () =>
+      Logging.info(`Server is running on port ${config.server.port}`)
+    );
+};
