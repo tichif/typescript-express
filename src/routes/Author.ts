c42c5f4f943e3881c23aae97b1850ae5dfcebc9e
Charles-Hébert Dalzon <charlby5@gmail.com>
2022-08-23 21:29:30 -0400

    Add data validation with Joi

--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 
 import controller from '../controllers/Author';
+import { Schemas, validateSchema } from '../middleware/validateSchema';
 
 const router = express.Router();
 
-router.post('/create', controller.createAuthor);
+router.post(
+  '/create',
+  validateSchema(Schemas.author.create),
+  controller.createAuthor
+);
 router.get('/get/:authorId', controller.readAuthor);
 router.get('/get', controller.readAllAuthors);
-router.patch('/update/:authorId', controller.updateAuthor);
+router.patch(
+  '/update/:authorId',
+  validateSchema(Schemas.author.update),
+  controller.updateAuthor
+);
 router.delete('/delete/:authorId', controller.deleteAuthor);
 
 export default router;
--- src/routes/Book.ts
+++ src/routes/Book.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 
 import controller from '../controllers/Book';
+import { Schemas, validateSchema } from '../middleware/validateSchema';
 
 const router = express.Router();
 
-router.post('/create', controller.createBook);
+router.post(
+  '/create',
+  validateSchema(Schemas.book.create),
+  controller.createBook
+);
 router.get('/get/:bookId', controller.readBook);
 router.get('/get', controller.readAllBooks);
-router.patch('/update/:bookId', controller.updateBook);
+router.patch(
+  '/update/:bookId',
+  validateSchema(Schemas.book.update),
+  controller.updateBook
+);
 router.delete('/delete/:bookId', controller.deleteBook);
 
 export default router;
